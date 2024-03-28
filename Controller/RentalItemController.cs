c75cd5b303ed5bcc83d5a64ae2d0d521fd504ef1
ac00587 <ac00587@westga.edu>
2024-03-28 13:59:40 -0400

    Add RentalItemDAL::GetMemberRentalItemHistory

--- Controller/RentalItemController.cs
+++ Controller/RentalItemController.cs
@@ -1,17 +1,20 @@
-﻿using CS6232_Group_6_Store.Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CS6232_Group_6_Store.DAL;
+using CS6232_Group_6_Store.Model;
 
 namespace CS6232_Group_6_Store.Controller
 {
     internal class RentalItemController
     {
+        private RentalItemDAL _rentalItemDAL;
+        
         internal List<RentalItem> GetMemberRentalItemHistory(int id)
         {
-            throw new NotImplementedException();
+            if (id < 1)
+            {
+                throw new ArgumentOutOfRangeException("id must be greater than 0");
+            }
+
+            return _rentalItemDAL.GetMemberRentalItemHistory(id);
         }
     }
 }
---
+++ DAL/RentalItemDAL.cs
@@ -0,0 +1,65 @@
+﻿using CS6232_Group_6_Store.Model;
+using System.Data;
+
+namespace CS6232_Group_6_Store.DAL
+{
+    internal class RentalItemDAL
+    {
+        internal List<RentalItem> GetMemberRentalItemHistory(int id)
+        {
+            if (id < 1)
+            {
+                throw new ArgumentOutOfRangeException("id must be greater than 0");
+            }
+
+            List<RentalItem> list = new List<RentalItem>();
+
+            using (var connection = DBConnection.GetConnection())
+            {
+                
+                connection.Open();
+
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "  SELECT * FROM rental_transactions as rt" 
+                        + " JOIN rental_items as ri ON rt.id = ri.transactionId" 
+                        + " JOIN furniture as f ON ri.furnitureId = f.id" 
+                        + " WHERE rt.memberId = @id;";
+
+                    command.Connection = connection;
+
+                    command.Parameters.Add("@id", SqlDbType.Int);
+                    command.Parameters["@id"].Value = id;
+
+                    using (var  reader = command.ExecuteReader())
+                    {
+                        int employeeIdOrdinal = reader.GetOrdinal("employeeId");
+                        int rentalDateOrdinal = reader.GetOrdinal("rentalDate");
+                        int dueDateOrdinal = reader.GetOrdinal("dueDate");
+                        int nameOrdinal = reader.GetOrdinal("name");
+                        int quantityOrdinal = reader.GetOrdinal("quantity");
+                        int rentalRateOrdinal = reader.GetOrdinal("rentalRate");
+                        int quantityReturnedOrdinal = reader.GetOrdinal("quantityReturned");
+                        
+                        while (reader.Read())
+                        {
+                            RentalItem item = new RentalItem();
+
+                            item.RentalTransaction.EmployeeId = reader.GetInt32(employeeIdOrdinal);
+                            item.RentalTransaction.RentalDate = reader.GetDateTime(rentalDateOrdinal);
+                            item.RentalTransaction.DueDate = reader.GetDateTime(dueDateOrdinal);
+                            item.Furniture.Name = reader.GetString(nameOrdinal);
+                            item.Quantity = reader.GetInt32(quantityOrdinal);
+                            item.Furniture.RentalRate = reader.GetInt32(rentalRateOrdinal);
+                            item.QuantityReturned = reader.GetInt32(quantityReturnedOrdinal);
+
+                            list.Add(item);
+                        }
+                    }
+                }
+            }
+
+            return list;
+        }
+    }
+}
