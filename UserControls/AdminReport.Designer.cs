71acc44515a0756bf8231759f483f542dbaa8cac
egrosch1 <egrosch1@my.westga.edu>
2024-04-19 12:18:19 -0400

    Added Design to Admin Report

---
+++ UserControls/AdminReport.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace CS6232_Group_6_Store.UserControls
+{
+    partial class AdminReport
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tableLayoutPanel1 = new TableLayoutPanel();
+            adminReportListView = new ListView();
+            tableLayoutPanel2 = new TableLayoutPanel();
+            startDateLabel = new Label();
+            endDateLabel = new Label();
+            adminReportButton = new Button();
+            startDatePicker = new DateTimePicker();
+            endDatePicker = new DateTimePicker();
+            tableLayoutPanel1.SuspendLayout();
+            tableLayoutPanel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Controls.Add(adminReportListView, 0, 1);
+            tableLayoutPanel1.Controls.Add(tableLayoutPanel2, 0, 0);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Size = new Size(784, 420);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // adminReportListView
+            // 
+            adminReportListView.Dock = DockStyle.Fill;
+            adminReportListView.Location = new Point(3, 213);
+            adminReportListView.Name = "adminReportListView";
+            adminReportListView.Size = new Size(778, 204);
+            adminReportListView.TabIndex = 0;
+            adminReportListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // tableLayoutPanel2
+            // 
+            tableLayoutPanel2.ColumnCount = 2;
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.Controls.Add(startDateLabel, 0, 0);
+            tableLayoutPanel2.Controls.Add(endDateLabel, 0, 1);
+            tableLayoutPanel2.Controls.Add(adminReportButton, 1, 2);
+            tableLayoutPanel2.Controls.Add(startDatePicker, 1, 0);
+            tableLayoutPanel2.Controls.Add(endDatePicker, 1, 1);
+            tableLayoutPanel2.Dock = DockStyle.Fill;
+            tableLayoutPanel2.Location = new Point(3, 3);
+            tableLayoutPanel2.Name = "tableLayoutPanel2";
+            tableLayoutPanel2.RowCount = 3;
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 33F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 33F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 34F));
+            tableLayoutPanel2.Size = new Size(778, 204);
+            tableLayoutPanel2.TabIndex = 1;
+            // 
+            // startDateLabel
+            // 
+            startDateLabel.AutoSize = true;
+            startDateLabel.Dock = DockStyle.Fill;
+            startDateLabel.Location = new Point(3, 0);
+            startDateLabel.Name = "startDateLabel";
+            startDateLabel.Size = new Size(383, 67);
+            startDateLabel.TabIndex = 0;
+            startDateLabel.Text = "Start Date";
+            startDateLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // endDateLabel
+            // 
+            endDateLabel.AutoSize = true;
+            endDateLabel.Dock = DockStyle.Fill;
+            endDateLabel.Location = new Point(3, 67);
+            endDateLabel.Name = "endDateLabel";
+            endDateLabel.Size = new Size(383, 67);
+            endDateLabel.TabIndex = 1;
+            endDateLabel.Text = "End Date";
+            endDateLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // adminReportButton
+            // 
+            adminReportButton.Dock = DockStyle.Fill;
+            adminReportButton.Location = new Point(392, 137);
+            adminReportButton.Name = "adminReportButton";
+            adminReportButton.Size = new Size(383, 64);
+            adminReportButton.TabIndex = 2;
+            adminReportButton.Text = "Generate Report";
+            adminReportButton.UseVisualStyleBackColor = true;
+            // 
+            // startDatePicker
+            // 
+            startDatePicker.Dock = DockStyle.Fill;
+            startDatePicker.Location = new Point(392, 3);
+            startDatePicker.Name = "startDatePicker";
+            startDatePicker.Size = new Size(383, 27);
+            startDatePicker.TabIndex = 3;
+            // 
+            // endDatePicker
+            // 
+            endDatePicker.Dock = DockStyle.Fill;
+            endDatePicker.Location = new Point(392, 70);
+            endDatePicker.Name = "endDatePicker";
+            endDatePicker.Size = new Size(383, 27);
+            endDatePicker.TabIndex = 4;
+            // 
+            // AdminReport
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(tableLayoutPanel1);
+            Name = "AdminReport";
+            Size = new Size(784, 420);
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel2.ResumeLayout(false);
+            tableLayoutPanel2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private ListView adminReportListView;
+        private TableLayoutPanel tableLayoutPanel2;
+        private Label startDateLabel;
+        private Label endDateLabel;
+        private Button adminReportButton;
+        private DateTimePicker startDatePicker;
+        private DateTimePicker endDatePicker;
+    }
+}
--- View/MainDashBoard.Designer.cs
+++ View/MainDashBoard.Designer.cs
@@ -34,18 +34,21 @@
             inventoryAndRentTabPage = new TabPage();
             inventoryRental1 = new UserControls.InventoryRental();
             returnTabPage = new TabPage();
-            tabPage1 = new TabPage();
+            inventoryReturn1 = new UserControls.InventoryReturn();
+            transactionManagementTab = new TabPage();
             transactionManagement1 = new UserControls.TransactionManagement();
+            adminReportTab = new TabPage();
+            adminReport1 = new UserControls.AdminReport();
             greetingLabel = new Label();
             logoutLinkLabel = new LinkLabel();
             userNameLabel = new Label();
             employeeNameLebel = new Label();
-            inventoryReturn1 = new UserControls.InventoryReturn();
             mainTabControl.SuspendLayout();
             customerTabPage.SuspendLayout();
             inventoryAndRentTabPage.SuspendLayout();
             returnTabPage.SuspendLayout();
-            tabPage1.SuspendLayout();
+            transactionManagementTab.SuspendLayout();
+            adminReportTab.SuspendLayout();
             SuspendLayout();
             // 
             // mainTabControl
@@ -53,21 +56,24 @@
             mainTabControl.Controls.Add(customerTabPage);
             mainTabControl.Controls.Add(inventoryAndRentTabPage);
             mainTabControl.Controls.Add(returnTabPage);
-            mainTabControl.Controls.Add(tabPage1);
+            mainTabControl.Controls.Add(transactionManagementTab);
+            mainTabControl.Controls.Add(adminReportTab);
             mainTabControl.Dock = DockStyle.Bottom;
-            mainTabControl.Location = new Point(0, 129);
+            mainTabControl.Location = new Point(0, 172);
+            mainTabControl.Margin = new Padding(3, 4, 3, 4);
             mainTabControl.Name = "mainTabControl";
             mainTabControl.SelectedIndex = 0;
-            mainTabControl.Size = new Size(1055, 493);
+            mainTabControl.Size = new Size(1206, 657);
             mainTabControl.TabIndex = 0;
             // 
             // customerTabPage
             // 
             customerTabPage.Controls.Add(memberManagement2);
-            customerTabPage.Location = new Point(4, 24);
+            customerTabPage.Location = new Point(4, 29);
+            customerTabPage.Margin = new Padding(3, 4, 3, 4);
             customerTabPage.Name = "customerTabPage";
-            customerTabPage.Padding = new Padding(3);
-            customerTabPage.Size = new Size(1047, 465);
+            customerTabPage.Padding = new Padding(3, 4, 3, 4);
+            customerTabPage.Size = new Size(1198, 624);
             customerTabPage.TabIndex = 0;
             customerTabPage.Text = "Customer";
             customerTabPage.UseVisualStyleBackColor = true;
@@ -75,19 +81,17 @@
             // memberManagement2
             // 
             memberManagement2.Dock = DockStyle.Fill;
-            memberManagement2.Location = new Point(3, 3);
-            memberManagement2.Margin = new Padding(3, 2, 3, 2);
+            memberManagement2.Location = new Point(3, 4);
             memberManagement2.Name = "memberManagement2";
-            memberManagement2.Size = new Size(1041, 459);
+            memberManagement2.Size = new Size(1192, 616);
             memberManagement2.TabIndex = 0;
             // 
             // inventoryAndRentTabPage
             // 
             inventoryAndRentTabPage.Controls.Add(inventoryRental1);
-            inventoryAndRentTabPage.Location = new Point(4, 24);
-            inventoryAndRentTabPage.Margin = new Padding(3, 2, 3, 2);
+            inventoryAndRentTabPage.Location = new Point(4, 29);
             inventoryAndRentTabPage.Name = "inventoryAndRentTabPage";
-            inventoryAndRentTabPage.Size = new Size(1047, 465);
+            inventoryAndRentTabPage.Size = new Size(192, 67);
             inventoryAndRentTabPage.TabIndex = 1;
             inventoryAndRentTabPage.Text = "Inventory and Rental Processing ";
             inventoryAndRentTabPage.UseVisualStyleBackColor = true;
@@ -96,38 +100,69 @@
             // 
             inventoryRental1.Dock = DockStyle.Fill;
             inventoryRental1.Location = new Point(0, 0);
+            inventoryRental1.Margin = new Padding(3, 5, 3, 5);
             inventoryRental1.Name = "inventoryRental1";
-            inventoryRental1.Size = new Size(1047, 465);
+            inventoryRental1.Size = new Size(192, 67);
             inventoryRental1.TabIndex = 0;
             // 
             // returnTabPage
             // 
             returnTabPage.Controls.Add(inventoryReturn1);
-            returnTabPage.Location = new Point(4, 24);
+            returnTabPage.Location = new Point(4, 29);
+            returnTabPage.Margin = new Padding(3, 4, 3, 4);
             returnTabPage.Name = "returnTabPage";
-            returnTabPage.Size = new Size(1047, 465);
+            returnTabPage.Size = new Size(192, 67);
             returnTabPage.TabIndex = 3;
             returnTabPage.Text = "Return Processing";
             returnTabPage.UseVisualStyleBackColor = true;
             // 
-            // tabPage1
+            // inventoryReturn1
             // 
-            tabPage1.Controls.Add(transactionManagement1);
-            tabPage1.Location = new Point(4, 24);
-            tabPage1.Name = "tabPage1";
-            tabPage1.Padding = new Padding(3);
-            tabPage1.Size = new Size(192, 72);
-            tabPage1.TabIndex = 2;
-            tabPage1.Text = "Transaction Management";
-            tabPage1.UseVisualStyleBackColor = true;
+            inventoryReturn1.Dock = DockStyle.Fill;
+            inventoryReturn1.Location = new Point(0, 0);
+            inventoryReturn1.Margin = new Padding(3, 5, 3, 5);
+            inventoryReturn1.Name = "inventoryReturn1";
+            inventoryReturn1.Size = new Size(192, 67);
+            inventoryReturn1.TabIndex = 0;
+            // 
+            // transactionManagementTab
+            // 
+            transactionManagementTab.Controls.Add(transactionManagement1);
+            transactionManagementTab.Location = new Point(4, 29);
+            transactionManagementTab.Margin = new Padding(3, 4, 3, 4);
+            transactionManagementTab.Name = "transactionManagementTab";
+            transactionManagementTab.Padding = new Padding(3, 4, 3, 4);
+            transactionManagementTab.Size = new Size(192, 67);
+            transactionManagementTab.TabIndex = 2;
+            transactionManagementTab.Text = "Transaction Management";
+            transactionManagementTab.UseVisualStyleBackColor = true;
             // 
             // transactionManagement1
             // 
             transactionManagement1.Dock = DockStyle.Fill;
-            transactionManagement1.Location = new Point(3, 3);
+            transactionManagement1.Location = new Point(3, 4);
+            transactionManagement1.Margin = new Padding(3, 5, 3, 5);
             transactionManagement1.Name = "transactionManagement1";
-            transactionManagement1.Size = new Size(186, 66);
+            transactionManagement1.Size = new Size(186, 59);
             transactionManagement1.TabIndex = 0;
+            // 
+            // adminReportTab
+            // 
+            adminReportTab.Controls.Add(adminReport1);
+            adminReportTab.Location = new Point(4, 29);
+            adminReportTab.Name = "adminReportTab";
+            adminReportTab.Size = new Size(1198, 624);
+            adminReportTab.TabIndex = 4;
+            adminReportTab.Text = "Admin Report";
+            adminReportTab.UseVisualStyleBackColor = true;
+            // 
+            // adminReport1
+            // 
+            adminReport1.Dock = DockStyle.Fill;
+            adminReport1.Location = new Point(0, 0);
+            adminReport1.Name = "adminReport1";
+            adminReport1.Size = new Size(1198, 624);
+            adminReport1.TabIndex = 0;
             // 
             // greetingLabel
             // 
@@ -135,16 +170,16 @@
             greetingLabel.Dock = DockStyle.Left;
             greetingLabel.Location = new Point(0, 0);
             greetingLabel.Name = "greetingLabel";
-            greetingLabel.Size = new Size(0, 15);
+            greetingLabel.Size = new Size(0, 20);
             greetingLabel.TabIndex = 1;
             // 
             // logoutLinkLabel
             // 
             logoutLinkLabel.AutoSize = true;
             logoutLinkLabel.Dock = DockStyle.Right;
-            logoutLinkLabel.Location = new Point(1002, 0);
+            logoutLinkLabel.Location = new Point(1141, 0);
             logoutLinkLabel.Name = "logoutLinkLabel";
-            logoutLinkLabel.Size = new Size(53, 15);
+            logoutLinkLabel.Size = new Size(65, 20);
             logoutLinkLabel.TabIndex = 2;
             logoutLinkLabel.TabStop = true;
             logoutLinkLabel.Text = "LOGOUT";
@@ -153,39 +188,32 @@
             // userNameLabel
             // 
             userNameLabel.AutoSize = true;
-            userNameLabel.Location = new Point(699, 7);
+            userNameLabel.Location = new Point(799, 9);
             userNameLabel.Name = "userNameLabel";
-            userNameLabel.Size = new Size(62, 15);
+            userNameLabel.Size = new Size(78, 20);
             userNameLabel.TabIndex = 6;
             userNameLabel.Text = "UserName";
             // 
             // employeeNameLebel
             // 
             employeeNameLebel.AutoSize = true;
-            employeeNameLebel.Location = new Point(466, 7);
+            employeeNameLebel.Location = new Point(533, 9);
             employeeNameLebel.Name = "employeeNameLebel";
-            employeeNameLebel.Size = new Size(91, 15);
+            employeeNameLebel.Size = new Size(115, 20);
             employeeNameLebel.TabIndex = 5;
             employeeNameLebel.Text = "EmployeeName";
             // 
-            // inventoryReturn1
-            // 
-            inventoryReturn1.Dock = DockStyle.Fill;
-            inventoryReturn1.Location = new Point(0, 0);
-            inventoryReturn1.Name = "inventoryReturn1";
-            inventoryReturn1.Size = new Size(1047, 465);
-            inventoryReturn1.TabIndex = 0;
-            // 
             // MainDashBoard
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1055, 622);
+            ClientSize = new Size(1206, 829);
             Controls.Add(userNameLabel);
             Controls.Add(employeeNameLebel);
             Controls.Add(logoutLinkLabel);
             Controls.Add(greetingLabel);
             Controls.Add(mainTabControl);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "MainDashBoard";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Main Dashboard";
@@ -194,7 +222,8 @@
             customerTabPage.ResumeLayout(false);
             inventoryAndRentTabPage.ResumeLayout(false);
             returnTabPage.ResumeLayout(false);
-            tabPage1.ResumeLayout(false);
+            transactionManagementTab.ResumeLayout(false);
+            adminReportTab.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -211,9 +240,11 @@
         private UserControls.MemberManagement memberManagement2;
         private TabPage inventoryAndRentTabPage;
         private UserControls.InventoryRental inventoryRental1;
-        private TabPage tabPage1;
+        private TabPage transactionManagementTab;
         private UserControls.TransactionManagement transactionManagement1;
         private TabPage returnTabPage;
         private UserControls.InventoryReturn inventoryReturn1;
+        private TabPage adminReportTab;
+        private UserControls.AdminReport adminReport1;
     }
 }
